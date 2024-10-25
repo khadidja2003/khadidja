e4cecf62aa591685abe4daf2bf27a77bae3db3eb
khadidja2003 <jihqnjihan682@gmail.com>
2024-10-25 18:22:12 +0200

    hello world

--- src/hel/Hello.java
+++ src/hel/Hello.java
@@ -3,7 +3,8 @@ package hel;
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			
+	        System.out.println("Je suis une version modifiée du code de l'exo 1");
 
 	}
 
